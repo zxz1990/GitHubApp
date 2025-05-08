f83905f31ac811aaeaebe641c52641173c02f150
zhuxuezhi <136934269@qq.com>
2025-05-08 10:20:51 +0800

    update ui

--- app/src/main/java/com/mcdull/githubapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mcdull/githubapp/ui/home/HomeFragment.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
-import com.google.gson.Gson
-import com.mcdull.githubapp.databinding.FragmentHomeBinding
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.mcdull.githubapp.R
+import com.mcdull.githubapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
